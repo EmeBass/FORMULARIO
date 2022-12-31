4b8bc05a4ded934dc80ad928ce0c5870186c2dc3
EmeBass <73545998+EmeBass@users.noreply.github.com>
2022-12-31 14:55:58 -0300

    Add files via upload

---
+++ Formulario/JavaScript/Script.js
@@ -0,0 +1,10 @@
+document.querySelector('#irAbajo').addEventListener('click', function(){
+    $(document).ready(function() {
+        $('a[href^="#"]').click(function() {
+          var destino = $(this.hash); //this.hash lee el atributo href de este
+          $('html, body').animate({ scrollTop: destino.offset().top }, 700); //Llega a su destino con el tiempo deseado
+          return false;
+        });
+      });
+    
+});
